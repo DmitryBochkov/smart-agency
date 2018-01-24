144f9fd96b91154835e91ea058956fa992c6a55f
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2018-01-24 03:55:57 +0200

    draft feedback slider added

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,25 @@
-//= jquery-1.12.4.min.js
-//= bootstrap.min.js
-
 $(document).ready(function() {
   var hamburger = document.querySelector(".hamburger");
   // On click
   hamburger.addEventListener("click", function() {
    // Toggle class "is-active"
    hamburger.classList.toggle("is-active");
+  });
+
+  $('#feedback-slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }]
   });
 });
